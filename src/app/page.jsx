7ba6916d0b95ed24c6c48e16b4e2dde9cf0ae02d
AshArce <arcebuchejohnashley@gmail.com>
2025-11-16 19:24:27 +0800

    3rd commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
+import React from 'react';
+import HeroPage from './components/HeroPage';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        
+        <HeroPage />
 
       <h1 className="text-4xl font-bold text-zinc-900 dark:text-white sm:text-5xl">
         Welcome to Next.js!
